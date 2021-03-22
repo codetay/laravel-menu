9b2ad5df12c2a4672b89b5e766848c13793f435a
#CODETAY <hi@developer.vn>
2021-03-22 17:16:16 +0700

    Update settings.php

--- src/config/settings.php
+++ src/config/settings.php
@@ -4,11 +4,12 @@ return array(
     'default' => array(
         'auto_activate' => true,
         'activate_parents' => true,
-        'active_class' => 'active',
+        'active_parent_class' => 'side-menu__sub-open',
+        'active_class' => 'side-menu--active',
         'restful' => false,
         'cascade_data' => true,
         'rest_base' => '',      // string|array
-        'active_element' => 'item',  // item|link
+        'active_element' => 'link',  // item|link
         'data_toggle_attribute' => 'data-toggle',
     ),
 );
